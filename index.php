a06edd7e3e98976f7a67632d4f7d37e00ae18fd4
James Ivan Frondarina <frondarinajames@gmail.com>
2026-04-27 12:09:59 +0800

    CCS0043L

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My php activity</title>
+</head>
+<body>
+    
+    <div class="header">
+        <h1>these are my PHP activities</h1>
+        <p>PHP is fun</p>
+    </div>
+</body>
+</html>
